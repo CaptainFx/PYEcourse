3c30ebd6da50a5945a56371a874a8e06e3364bfb
peter hu <peterhu5916@gmail.com>
2019-03-29 08:54:35 +0800

    try except

--- guessnumber.py
+++ guessnumber.py
@@ -1,5 +1,8 @@
-guess = eval(input())
-if guess > 66 or guess < 66:
-    print("猜错了")
-else:
-    print("猜对了")
+try:
+    guess = eval(input())
+    if guess > 66 or guess < 66:
+        print("猜错了")
+    else:
+        print("猜对了")
+except:
+    print("输入的不是数字")
